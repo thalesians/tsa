b25b4c081d1b9fdd1bd30ca518a1eed44b38fed1
Paul Bilokon <paul@thalesians.com>
2017-06-21 14:41:39 +0100

    Added another unit test for Kalman filtering.

--- src/test/python/thalesians/tsa/testfiltering.py
+++ src/test/python/thalesians/tsa/testfiltering.py
@@ -19,17 +19,17 @@ class TestFiltering(unittest.TestCase):
         
         t1 = t0 + dt.timedelta(hours=1)
         
-        predictedobs1 = observable.predict(t1)
-        npt.assert_almost_equal(predictedobs1.distr.mean, 100. + 3./24.)
-        npt.assert_almost_equal(predictedobs1.distr.cov, 250. + 25./24.)
-        npt.assert_almost_equal(predictedobs1.crosscov, predictedobs1.distr.cov)
+        priorpredictedobs1 = observable.predict(t1)
+        npt.assert_almost_equal(priorpredictedobs1.distr.mean, 100. + 3./24.)
+        npt.assert_almost_equal(priorpredictedobs1.distr.cov, 250. + 25./24.)
+        npt.assert_almost_equal(priorpredictedobs1.crosscov, priorpredictedobs1.distr.cov)
         
         observable.observe(t1, N(mean=100.35, cov=100.0))
         
-        predictedobs2 = observable.predict(t1)
-        npt.assert_almost_equal(predictedobs2.distr.mean, 100.28590504)
-        npt.assert_almost_equal(predictedobs2.distr.cov, 71.513353115)
-        npt.assert_almost_equal(predictedobs2.crosscov, predictedobs2.distr.cov)
+        posteriorpredictedobs1 = observable.predict(t1)
+        npt.assert_almost_equal(posteriorpredictedobs1.distr.mean, 100.28590504)
+        npt.assert_almost_equal(posteriorpredictedobs1.distr.cov, 71.513353115)
+        npt.assert_almost_equal(posteriorpredictedobs1.crosscov, posteriorpredictedobs1.distr.cov)
         
         t2 = t1 + dt.timedelta(hours=2)
         
@@ -45,6 +45,33 @@ class TestFiltering(unittest.TestCase):
         npt.assert_almost_equal(posteriorpredictedobs2.distr.cov, 42.395213845)
         npt.assert_almost_equal(posteriorpredictedobs2.crosscov, posteriorpredictedobs2.distr.cov)
     
+    def testkalmanfilterwithoutpriorpredict(self):
+        t0 = dt.datetime(2014, 2, 12, 16, 18, 25, 204000)
+        
+        process = proc.WienerProcess.createfromcov(mean=3., cov=25.)
+        
+        kf = filtering.KalmanFilter(t0, statedistr=N(mean=100., cov=250.), process=process)
+                
+        observable = kf.createobservable(filtering.KalmanFilterObsModel.createfromcompoundobsmatrix(1.), process)
+        
+        t1 = t0 + dt.timedelta(hours=1)
+        
+        observable.observe(t1, N(mean=100.35, cov=100.0))
+
+        posteriorpredictedobs1 = observable.predict(t1)
+        npt.assert_almost_equal(posteriorpredictedobs1.distr.mean, 100.28590504)
+        npt.assert_almost_equal(posteriorpredictedobs1.distr.cov, 71.513353115)
+        npt.assert_almost_equal(posteriorpredictedobs1.crosscov, posteriorpredictedobs1.distr.cov)
+        
+        t2 = t1 + dt.timedelta(hours=2)
+        
+        observable.observe(t2, N(mean=100.35, cov=100.0))
+        
+        posteriorpredictedobs2 = observable.predict(t2)
+        npt.assert_almost_equal(posteriorpredictedobs2.distr.mean, 100.45709020)
+        npt.assert_almost_equal(posteriorpredictedobs2.distr.cov, 42.395213845)
+        npt.assert_almost_equal(posteriorpredictedobs2.crosscov, posteriorpredictedobs2.distr.cov)
+
 if __name__ == '__main__':
     unittest.main()
     
