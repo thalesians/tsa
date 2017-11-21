3b8fe42932a8a3dd8d5687317771843c0a0d6a8c
Paul Bilokon <paul@thalesians.com>
2017-11-21 18:24:22 +0000

    Pypes now work as iterators.

--- src/main/python/thalesians/tsa/pypes.py
+++ src/main/python/thalesians/tsa/pypes.py
@@ -16,9 +16,15 @@ class Pype(object):
     def __init__(self, direction, name=None, host=None, port=22184):
         if name is None: name = random.choice(string.ascii_uppercase) + \
                 ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(3))
+        checks.check_string(name, allow_none=False)
         checks.check(lambda: name.isalnum(), 'Pype\'s name is not alphanumeric')
+        checks.check(lambda: len(name) > 0, 'Pype\'s name is an empty string')
+        
         self._name = name
-        self._name_bytes = (name + ' ').encode('utf-8')
+        self._main_topic = self._name
+        self._main_topic_bytes = (self._main_topic + ' ').encode('utf-8')
+        self._eof_topic = self._name + '!'
+        self._eof_topic_bytes = (self._eof_topic + ' ').encode('utf-8')
         
         self._port = port
         self._context = zmq.Context()
@@ -28,6 +34,7 @@ class Pype(object):
             self._socket = self._context.socket(zmq.SUB)
             self._socket.connect('tcp://%s:%d' % (self._host, self._port))
             self._socket.setsockopt_string(zmq.SUBSCRIBE, self._name)
+            self._socket.setsockopt_string(zmq.SUBSCRIBE, self._eof_topic)
         elif direction == Direction.OUTGOING:
             if host is None: host = '*'
             self._host = host
@@ -36,19 +43,52 @@ class Pype(object):
         else:
             raise ValueError('Unexpected direction: %s' % str(direction))
         self._direction = direction
+        self._closed = False
+        
+    def close(self):
+        if not self._closed:
+            if self._direction == Direction.OUTGOING:
+                self._socket.send(self._eof_topic_bytes)
+            self._socket.close()
+            self._context.term()
+            self._closed = True
+        
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, type, value, traceback):
+        self.close()
         
     def send(self, obj):
-        ba = bytearray(self._name_bytes)
+        if self._closed: raise ValueError('I/O operation on closed pype')
+        ba = bytearray(self._main_topic_bytes)
         p = pickle.dumps(obj, protocol=-1)
         z = zlib.compress(p)
         ba.extend(z)
         return self._socket.send(ba, flags=0)
     
-    def receive(self):
+    def receive(self, notify_of_eof=False):
+        if self._closed: raise ValueError('I/O operation on closed pype')
         s = self._socket.recv()
-        _, data = s.split(b' ', 1)
-        p = zlib.decompress(data)
-        o = pickle.loads(p)
+        topic, data = s.split(b' ', 1)
+        topic = topic.decode("utf-8")
+        if topic == self._eof_topic:
+            o = None
+            eof = True
+            self.close()
+        else:
+            p = zlib.decompress(data)
+            o = pickle.loads(p)
+            eof = False
+        return (o, eof) if notify_of_eof else o
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self._closed: raise StopIteration
+        o, eof = self.receive(notify_of_eof=True)
+        if eof: raise StopIteration
         return o
         
     @property
@@ -66,6 +106,10 @@ class Pype(object):
     @property
     def port(self):
         return self._port
+    
+    @property
+    def closed(self):
+        return self._closed
     
     def __str__(self):
         return 'Pype(name="%s", direction=%s, host="%s", port=%d)' % (self._name, self._direction, self._host, self._port)
