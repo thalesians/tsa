c29e359979390b335f654259c2c24aa37a846828
Paul Bilokon <paul@thalesians.com>
2018-04-16 11:02:29 +0100

    Grid search visualisation is now (optionally) interactive.

--- src/main/python/thalesians/tsa/optimisation/visual.py
+++ src/main/python/thalesians/tsa/optimisation/visual.py
@@ -9,10 +9,11 @@ import thalesians.tsa.checks as checks
 import thalesians.tsa.numpyutils as npu
 import thalesians.tsa.utils as utils
 
-def visualise_grid_search(grid_search_output, aggregate_function=np.nanmean, fig=None, title=None):
+def visualise_grid_search(grid_search_output, aggregate_function=np.nanmean, fig=None, title=None, refresh_until_ready=False):
     if fig is None: fig = plt.figure()
 
     if title is None: title = grid_search_output.optimisation_id
+    fig.suptitle(title)
 
     param_names = list(grid_search_output.param_ranges.keys())
 
@@ -46,19 +47,27 @@ def visualise_grid_search(grid_search_output, aggregate_function=np.nanmean, fig
             if i1 == len(param_names) - 1: ax.set_xlabel(param_name2)
             if i2 == 0: ax.set_ylabel(param_name1)
 
-    for status in grid_search_output.evaluation_statuses:
-        if status.ready:
-            checks.check(utils.sequence_eq(param_names, status.work.info['param_names']))
-            param_value_index_combinations = itertools.combinations(range(len(param_names)), 2)
-            param_value_index_combinations = [(i2, i1) for (i1, i2) in param_value_index_combinations if i1 != i2]
-            for i1, i2 in param_value_index_combinations:
-                param_value_index1 = status.work.info['param_value_indices'][i1]
-                param_value_index2 = status.work.info['param_value_indices'][i2]
-                datas[(i1, i2)][param_value_index1, param_value_index2].append(status.result.result)
-                new_data = npu.apply(np.nanmean, datas[(i1, i2)])
-                heatmaps[(i1, i2)].set_data(new_data)
-                heatmaps[(i1, i2)].autoscale()
-
-    fig.suptitle(title)
+    while True:
+        all_ready = True
+        for status in grid_search_output.evaluation_statuses:
+            if not status.ready: all_ready = False
+            else:
+                checks.check(utils.sequence_eq(param_names, status.work.info['param_names']))
+                param_value_index_combinations = itertools.combinations(range(len(param_names)), 2)
+                param_value_index_combinations = [(i2, i1) for (i1, i2) in param_value_index_combinations if i1 != i2]
+                for i1, i2 in param_value_index_combinations:
+                    param_value_index1 = status.work.info['param_value_indices'][i1]
+                    param_value_index2 = status.work.info['param_value_indices'][i2]
+                    datas[(i1, i2)][param_value_index1, param_value_index2].append(status.result.result)
+                    new_data = npu.apply(np.nanmean, datas[(i1, i2)])
+                    heatmaps[(i1, i2)].set_data(new_data)
+                    heatmaps[(i1, i2)].autoscale()
+        if (not refresh_until_ready) or all_ready: break
+        else:
+            fig.canvas.draw_idle()
+            try:
+                fig.canvas.flush_events()
+            except NotImplementedError:
+                fig.canvas.draw()
 
     return fig
