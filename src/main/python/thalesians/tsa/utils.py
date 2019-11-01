680afe737b0aea91be8f25f283cc1689f9440ae7
Paul Bilokon <paul@thalesians.com>
2019-11-01 17:43:21 +0000

    Added doctests to utils.py.

--- src/main/python/thalesians/tsa/utils.py
+++ src/main/python/thalesians/tsa/utils.py
@@ -7,12 +7,35 @@ import operator
 import thalesians.tsa.intervals
 
 def sequence_eq(sequence1, sequence2):
+    """
+    Compares two sequences.
+
+    Parameters
+    ----------
+    sequence1 : sequence
+        The first sequence.
+    sequence2 : sequence
+        The second sequence.
+
+    Returns
+    -------
+    bool
+        `True` iff `sequence1` equals `sequence2`, otherwise `False`.
+    """
     return len(sequence1) == len(sequence2) and all(map(operator.eq, sequence1, sequence2))
 
 def cmp(x, y):
     return (x > y) - (x < y)
 
 def most_common(iterable):
+    """
+    >>> most_common(['foo', 'bar', 'bar', 'foo', 'bar'])
+    'bar'
+    >>> most_common(['foo', 'bar', 'bar', 'foo'])
+    'foo'
+    >>> most_common(['foo', 'bar'])
+    'foo'
+    """
     sorted_iterable = sorted((x, i) for i, x in enumerate(iterable))
     groups = itertools.groupby(sorted_iterable, key=operator.itemgetter(0))
     def _auxfun(g):
@@ -72,14 +95,52 @@ def xconst(value):
     while True: yield value
 
 def xbatch(size, iterable):
+    """
+    >>> list(xbatch(2, range(10)))
+    [range(0, 2), range(2, 4), range(4, 6), range(6, 8), range(8, 10)]
+    >>> list(xbatch(3, ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']))
+    [['Jan', 'Feb', 'Mar'], ['Apr', 'May', 'Jun'], ['Jul', 'Aug', 'Sep'], ['Oct', 'Nov', 'Dec']]
+    >>> list(xbatch(3, ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')))
+    [('Jan', 'Feb', 'Mar'), ('Apr', 'May', 'Jun'), ('Jul', 'Aug', 'Sep'), ('Oct', 'Nov', 'Dec')]
+    >>> import numpy as np
+    >>> list(xbatch(2, np.array(range(10))))
+    [array([0, 1]), array([2, 3]), array([4, 5]), array([6, 7]), array([8, 9])]
+    >>> list(xbatch(2, range(10)))
+    [range(0, 2), range(2, 4), range(4, 6), range(6, 8), range(8, 10)]
+    """
     l = len(iterable)
     for i in range(0, l, size):
         yield iterable[i:min(i + size, l)]
 
 def batch(size, iterable):
+    """
+    >>> batch(2, range(10))
+    [range(0, 2), range(2, 4), range(4, 6), range(6, 8), range(8, 10)]
+    >>> batch(3, [429, 5, 2, 14, 42, 132, 1, 1])
+    [[429, 5, 2], [14, 42, 132], [1, 1]]
+    >>> batch(4, range(10))
+    [range(0, 4), range(4, 8), range(8, 10)]
+    """
     return list(xbatch(size, iterable))
 
 def peek(iterable, size=1):
+    """
+    >>> it = xbatch(2, range(10))
+    >>> first_three, new_it = peek(it, 3)
+    >>> first_three
+    [range(0, 2), range(2, 4), range(4, 6)]
+    >>> list(new_it)
+    [range(0, 2), range(2, 4), range(4, 6), range(6, 8), range(8, 10)]
+    >>> list(it)
+    []
+        
+    >>> it = xbatch(2, range(10))
+    >>> first_three, new_it = peek(it, 3)
+    >>> first_three
+    [range(0, 2), range(2, 4), range(4, 6)]
+    >>> list(it)
+    [range(6, 8), range(8, 10)]
+    """
     objs = []
     for _ in range(size):
         try:
@@ -90,6 +151,44 @@ def peek(iterable, size=1):
     return objs, itertools.chain(objs, iterable)
 
 def intervals(start, end, delta, intervals_right_closed=False):
+    """
+    >>> intervals(start=0, end=15, delta=5, intervals_right_closed=False)
+    [[0, 5), [5, 10), [10, 15)]
+    
+    >>> intervals(start=0, end=15, delta=5, intervals_right_closed=True)
+    [(0, 5], (5, 10], (10, 15]]
+    
+    >>> intervals(start=0, end=15, delta=4, intervals_right_closed=False)
+    [[0, 4), [4, 8), [8, 12), [12, 15)]
+    
+    >>> intervals(start=0, end=15, delta=4, intervals_right_closed=True)
+    [(0, 4], (4, 8], (8, 12], (12, 15]]
+    
+    >>> import datetime as dt
+    >>> intervals(start=dt.date(2019, 8, 31), end=dt.date(2019, 9, 15), delta=dt.timedelta(days=5), intervals_right_closed=False)
+    [[2019-08-31, 2019-09-05), [2019-09-05, 2019-09-10), [2019-09-10, 2019-09-15)]
+    
+    >>> intervals(start=dt.date(2019, 8, 31), end=dt.date(2019, 9, 15), delta=dt.timedelta(days=5), intervals_right_closed=True)
+    [(2019-08-31, 2019-09-05], (2019-09-05, 2019-09-10], (2019-09-10, 2019-09-15]]
+    
+    >>> intervals(start=dt.date(2019, 8, 31), end=dt.date(2019, 9, 15), delta=dt.timedelta(days=4), intervals_right_closed=False)
+    [[2019-08-31, 2019-09-04), [2019-09-04, 2019-09-08), [2019-09-08, 2019-09-12), [2019-09-12, 2019-09-15)]
+    
+    >>> intervals(start=dt.date(2019, 8, 31), end=dt.date(2019, 9, 15), delta=dt.timedelta(days=4), intervals_right_closed=True)
+    [(2019-08-31, 2019-09-04], (2019-09-04, 2019-09-08], (2019-09-08, 2019-09-12], (2019-09-12, 2019-09-15]]
+    
+    >>> intervals(start=dt.datetime(2019, 10, 8, 0), end=dt.datetime(2019, 10, 8, 15), delta=dt.timedelta(hours=5), intervals_right_closed=False)
+    [[2019-10-08 00:00:00, 2019-10-08 05:00:00), [2019-10-08 05:00:00, 2019-10-08 10:00:00), [2019-10-08 10:00:00, 2019-10-08 15:00:00)]
+    
+    >>> intervals(start=dt.datetime(2019, 10, 8, 0), end=dt.datetime(2019, 10, 8, 15), delta=dt.timedelta(hours=5), intervals_right_closed=True)
+    [(2019-10-08 00:00:00, 2019-10-08 05:00:00], (2019-10-08 05:00:00, 2019-10-08 10:00:00], (2019-10-08 10:00:00, 2019-10-08 15:00:00]]
+    
+    >>> intervals(start=dt.datetime(2019, 10, 8, 0), end=dt.datetime(2019, 10, 8, 15), delta=dt.timedelta(hours=4), intervals_right_closed=False)
+    [[2019-10-08 00:00:00, 2019-10-08 04:00:00), [2019-10-08 04:00:00, 2019-10-08 08:00:00), [2019-10-08 08:00:00, 2019-10-08 12:00:00), [2019-10-08 12:00:00, 2019-10-08 15:00:00)]
+    
+    >>> intervals(start=dt.datetime(2019, 10, 8, 0), end=dt.datetime(2019, 10, 8, 15), delta=dt.timedelta(hours=4), intervals_right_closed=True)
+    [(2019-10-08 00:00:00, 2019-10-08 04:00:00], (2019-10-08 04:00:00, 2019-10-08 08:00:00], (2019-10-08 08:00:00, 2019-10-08 12:00:00], (2019-10-08 12:00:00, 2019-10-08 15:00:00]]
+    """
     result = []
     interval_start, interval_end = None, None
     while True:
@@ -119,6 +218,86 @@ class Bracket(object):
         return str(self)
 
 def bracket(iterable, origin, interval_size, already_sorted=False, intervals_right_closed=False, coalesce=False):
+    """
+    >>> data = [8, 11, 12, 13, 14, 27, 29, 37, 49, 50, 51, 79, 85]
+    
+    >>> brackets, bracket_indices = bracket(data, 3, 5)
+    >>> brackets
+    [{[8, 13), 1}, {[13, 18), 2}, {[23, 28), 4}, {[28, 33), 5}, {[33, 38), 6}, {[48, 53), 9}, {[78, 83), 15}, {[83, 88), 16}]
+    >>> bracket_indices
+    [0, 0, 0, 1, 1, 2, 3, 4, 5, 5, 5, 6, 7]
+    
+    >>> brackets, bracket_indices = bracket(data, 3, 5, intervals_right_closed=True)
+    >>> brackets
+    [{(3, 8], 0}, {(8, 13], 1}, {(13, 18], 2}, {(23, 28], 4}, {(28, 33], 5}, {(33, 38], 6}, {(48, 53], 9}, {(78, 83], 15}, {(83, 88], 16}]
+    >>> bracket_indices
+    [0, 1, 1, 1, 2, 3, 4, 5, 6, 6, 6, 7, 8]
+
+    >>> brackets, bracket_indices = bracket(data, 3, 5, coalesce=True)
+    >>> brackets
+    [{[8, 18), 1}, {[23, 38), 4}, {[48, 53), 9}, {[78, 88), 15}]
+    >>> bracket_indices
+    [0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3]
+
+    >>> brackets, bracket_indices = bracket(data, 3, 5, intervals_right_closed=True, coalesce=True)
+    >>> brackets
+    [{(3, 18], 0}, {(23, 38], 4}, {(48, 53], 9}, {(78, 88], 15}]
+    >>> bracket_indices
+    [0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3]
+    
+    >>> import datetime as dt
+    >>> data = [dt.date(2017, 1, 31) + dt.timedelta(days=x) for x in [8, 11, 12, 13, 14, 27, 29, 37, 49, 50, 51, 79, 85]];
+    
+    >>> brackets, bracket_indices = bracket(data, dt.date(2017, 2, 3), dt.timedelta(days=5))
+    >>> brackets
+    [{[2017-02-08, 2017-02-13), 1}, {[2017-02-13, 2017-02-18), 2}, {[2017-02-23, 2017-02-28), 4}, {[2017-02-28, 2017-03-05), 5}, {[2017-03-05, 2017-03-10), 6}, {[2017-03-20, 2017-03-25), 9}, {[2017-04-19, 2017-04-24), 15}, {[2017-04-24, 2017-04-29), 16}]
+    >>> bracket_indices
+    [0, 0, 0, 1, 1, 2, 3, 4, 5, 5, 5, 6, 7]
+
+    >>> brackets, bracket_indices = bracket(data, dt.date(2017, 2, 3), dt.timedelta(days=5), intervals_right_closed=True)
+    >>> brackets
+    [{(2017-02-03, 2017-02-08], 0}, {(2017-02-08, 2017-02-13], 1}, {(2017-02-13, 2017-02-18], 2}, {(2017-02-23, 2017-02-28], 4}, {(2017-02-28, 2017-03-05], 5}, {(2017-03-05, 2017-03-10], 6}, {(2017-03-20, 2017-03-25], 9}, {(2017-04-19, 2017-04-24], 15}, {(2017-04-24, 2017-04-29], 16}]
+    >>> bracket_indices
+    [0, 1, 1, 1, 2, 3, 4, 5, 6, 6, 6, 7, 8]
+
+    >>> brackets, bracket_indices = bracket(data, dt.date(2017, 2, 3), dt.timedelta(days=5), coalesce=True)
+    >>> brackets
+    [{[2017-02-08, 2017-02-18), 1}, {[2017-02-23, 2017-03-10), 4}, {[2017-03-20, 2017-03-25), 9}, {[2017-04-19, 2017-04-29), 15}]
+    >>> bracket_indices
+    [0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3]
+
+    >>> brackets, bracket_indices = bracket(data, dt.date(2017, 2, 3), dt.timedelta(days=5), intervals_right_closed=True, coalesce=True)
+    >>> brackets
+    [{(2017-02-03, 2017-02-18], 0}, {(2017-02-23, 2017-03-10], 4}, {(2017-03-20, 2017-03-25], 9}, {(2017-04-19, 2017-04-29], 15}]
+    >>> bracket_indices
+    [0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3]
+
+    >>> data = [dt.datetime(2017, 1, 31, 0, 0, 0) + dt.timedelta(minutes=x) for x in [8, 11, 12, 13, 14, 27, 29, 37, 49, 50, 51, 79, 85]];
+
+    >>> brackets, bracket_indices = bracket(data, dt.datetime(2017, 1, 31, 0, 3, 0), dt.timedelta(minutes=5))
+    >>> brackets
+    [{[2017-01-31 00:08:00, 2017-01-31 00:13:00), 1}, {[2017-01-31 00:13:00, 2017-01-31 00:18:00), 2}, {[2017-01-31 00:23:00, 2017-01-31 00:28:00), 4}, {[2017-01-31 00:28:00, 2017-01-31 00:33:00), 5}, {[2017-01-31 00:33:00, 2017-01-31 00:38:00), 6}, {[2017-01-31 00:48:00, 2017-01-31 00:53:00), 9}, {[2017-01-31 01:18:00, 2017-01-31 01:23:00), 15}, {[2017-01-31 01:23:00, 2017-01-31 01:28:00), 16}]
+    >>> bracket_indices
+    [0, 0, 0, 1, 1, 2, 3, 4, 5, 5, 5, 6, 7]
+
+    >>> brackets, bracket_indices = bracket(data, dt.datetime(2017, 1, 31, 0, 3, 0), dt.timedelta(minutes=5), intervals_right_closed=True)
+    >>> brackets
+    [{(2017-01-31 00:03:00, 2017-01-31 00:08:00], 0}, {(2017-01-31 00:08:00, 2017-01-31 00:13:00], 1}, {(2017-01-31 00:13:00, 2017-01-31 00:18:00], 2}, {(2017-01-31 00:23:00, 2017-01-31 00:28:00], 4}, {(2017-01-31 00:28:00, 2017-01-31 00:33:00], 5}, {(2017-01-31 00:33:00, 2017-01-31 00:38:00], 6}, {(2017-01-31 00:48:00, 2017-01-31 00:53:00], 9}, {(2017-01-31 01:18:00, 2017-01-31 01:23:00], 15}, {(2017-01-31 01:23:00, 2017-01-31 01:28:00], 16}]
+    >>> bracket_indices
+    [0, 1, 1, 1, 2, 3, 4, 5, 6, 6, 6, 7, 8]
+
+    >>> brackets, bracket_indices = bracket(data, dt.datetime(2017, 1, 31, 0, 3, 0), dt.timedelta(minutes=5), coalesce=True)
+    >>> brackets
+    [{[2017-01-31 00:08:00, 2017-01-31 00:18:00), 1}, {[2017-01-31 00:23:00, 2017-01-31 00:38:00), 4}, {[2017-01-31 00:48:00, 2017-01-31 00:53:00), 9}, {[2017-01-31 01:18:00, 2017-01-31 01:28:00), 15}]
+    >>> bracket_indices
+    [0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3]
+
+    >>> brackets, bracket_indices = bracket(data, dt.datetime(2017, 1, 31, 0, 3, 0), dt.timedelta(minutes=5), intervals_right_closed=True, coalesce=True)
+    >>> brackets
+    [{(2017-01-31 00:03:00, 2017-01-31 00:18:00], 0}, {(2017-01-31 00:23:00, 2017-01-31 00:38:00], 4}, {(2017-01-31 00:48:00, 2017-01-31 00:53:00], 9}, {(2017-01-31 01:18:00, 2017-01-31 01:28:00], 15}]
+    >>> bracket_indices
+    [0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3]
+    """
     if not already_sorted:
         sorted_indices, iterable = zip(*sorted([(i, v) for i, v in enumerate(iterable)], key=operator.itemgetter(1)))
     
@@ -253,6 +432,161 @@ class FlatStoredArray(object):
         return FlatStoredArray.__ItemsIterator(self._data, self)
     
 class DiagonalArray(FlatStoredArray):
+    """
+    >>> a = DiagonalArray(5)
+    
+    >>> a[0,0] = 0
+    >>> a[1,0], a[1,1] = 10, 20
+    >>> a[2,0], a[2,1], a[2,2] = 30, 40, 50
+    >>> a[3,0], a[3,1], a[3,2], a[3,3] = 60, 70, 80, 90
+    >>> a[4,0], a[4,1], a[4,2], a[4,3], a[4,4] = 100, 110, 120, 130, 140
+    
+    >>> len(a)
+    15
+    
+    >>> a[0,0]
+    0
+    >>> a[1,0]
+    10
+    >>> a[1,1]
+    20
+    >>> a[2,0]
+    30
+    >>> a[2,1]
+    40
+    >>> a[2,2]
+    50
+    >>> a[3,0]
+    60
+    >>> a[3,1]
+    70
+    >>> a[3,2]
+    80
+    >>> a[3,3]
+    90
+    >>> a[4,0]
+    100
+    >>> a[4,1]
+    110
+    >>> a[4,2]
+    120
+    >>> a[4,3]
+    130
+    >>> a[4,4]
+    140
+    
+    >>> a[0,0]
+    0
+    >>> a[0,1]
+    10
+    >>> a[1,1]
+    20
+    >>> a[0,2]
+    30
+    >>> a[1,2]
+    40
+    >>> a[2,2]
+    50
+    >>> a[0,3]
+    60
+    >>> a[1,3]
+    70
+    >>> a[2,3]
+    80
+    >>> a[3,3]
+    90
+    >>> a[0,4]
+    100
+    >>> a[1,4]
+    110
+    >>> a[2,4]
+    120
+    >>> a[3,4]
+    130
+    >>> a[4,4]
+    140
+    
+    >>> a._indextokey(0)
+    (0, 0)
+    >>> a._indextokey(1)
+    (1, 0)
+    >>> a._indextokey(2)
+    (1, 1)
+    >>> a._indextokey(3)
+    (2, 0)
+    >>> a._indextokey(4)
+    (2, 1)
+    >>> a._indextokey(5)
+    (2, 2)
+    >>> a._indextokey(6)
+    (3, 0)
+    >>> a._indextokey(7)
+    (3, 1)
+    >>> a._indextokey(8)
+    (3, 2)
+    >>> a._indextokey(9)
+    (3, 3)
+    >>> a._indextokey(10)
+    (4, 0)
+    >>> a._indextokey(11)
+    (4, 1)
+    >>> a._indextokey(12)
+    (4, 2)
+    >>> a._indextokey(13)
+    (4, 3)
+    >>> a._indextokey(14)
+    (4, 4)
+
+    >>> values = []
+    >>> for v in a: values.append(v)
+    >>> tuple(a)
+    (0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140)
+    
+    >>> keys = []
+    >>> for k in a.keys(): keys.append(k)
+    >>> keys
+    [(0, 0), (1, 0), (1, 1), (2, 0), (2, 1), (2, 2), (3, 0), (3, 1), (3, 2), (3, 3), (4, 0), (4, 1), (4, 2), (4, 3), (4, 4)]
+
+    >>> keys, values = [], []
+    >>> for k, v in a.items():
+    ...     keys.append(k)
+    ...     values.append(v)
+    >>> keys
+    [(0, 0), (1, 0), (1, 1), (2, 0), (2, 1), (2, 2), (3, 0), (3, 1), (3, 2), (3, 3), (4, 0), (4, 1), (4, 2), (4, 3), (4, 4)]
+    >>> values
+    [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140]
+    
+    >>> a.mindim(1)
+    1
+    >>> a.mindim(2)
+    2
+    >>> a.mindim(3)
+    2
+    >>> a.mindim(4)
+    3
+    >>> a.mindim(5)
+    3
+    >>> a.mindim(6)
+    3
+    >>> a.mindim(7)
+    4
+    >>> a.mindim(8)
+    4
+    >>> a.mindim(9)
+    4
+    >>> a.mindim(10)
+    4
+    >>> a.mindim(11)
+    5
+    >>> a.mindim(12)
+    5
+    >>> a.mindim(13)
+    5
+    >>> a.mindim(14)
+    5
+    >>> a.mindim(15)
+    5
+    """
     def __init__(self, dim):
         super(DiagonalArray, self).__init__(dim)
         self.__dim = dim
@@ -316,6 +650,119 @@ class DiagonalArray(FlatStoredArray):
         return res
         
 class SubdiagonalArray(FlatStoredArray):
+    """
+    >>> a = SubdiagonalArray(5)
+    >>> a[1,0] = 0
+    >>> a[2,0], a[2,1] = 10, 20
+    >>> a[3,0], a[3,1], a[3,2] = 30, 40, 50
+    >>> a[4,0], a[4,1], a[4,2], a[4,3] = 60, 70, 80, 90
+    
+    >>> len(a)
+    10
+    
+    >>> a[1,0]
+    0
+    >>> a[2,0]
+    10
+    >>> a[2,1]
+    20
+    >>> a[3,0]
+    30
+    >>> a[3,1]
+    40
+    >>> a[3,2]
+    50
+    >>> a[4,0]
+    60
+    >>> a[4,1]
+    70
+    >>> a[4,2]
+    80
+    >>> a[4,3]
+    90
+    
+    >>> a[0,1]
+    0
+    >>> a[0,2]
+    10
+    >>> a[1,2]
+    20
+    >>> a[0,3]
+    30
+    >>> a[1,3]
+    40
+    >>> a[2,3]
+    50
+    >>> a[0,4]
+    60
+    >>> a[1,4]
+    70
+    >>> a[2,4]
+    80
+    >>> a[3,4]
+    90
+    
+    >>> a._indextokey(0)
+    (1, 0)
+    >>> a._indextokey(1)
+    (2, 0)
+    >>> a._indextokey(2)
+    (2, 1)
+    >>> a._indextokey(3)
+    (3, 0)
+    >>> a._indextokey(4)
+    (3, 1)
+    >>> a._indextokey(5)
+    (3, 2)
+    >>> a._indextokey(6)
+    (4, 0)
+    >>> a._indextokey(7)
+    (4, 1)
+    >>> a._indextokey(8)
+    (4, 2)
+    >>> a._indextokey(9)
+    (4, 3)
+    
+    >>> values = []
+    >>> for v in a: values.append(v)
+    >>> values
+    [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+
+    >>> keys = []
+    >>> for k in a.keys(): keys.append(k)
+    >>> keys
+    [(1, 0), (2, 0), (2, 1), (3, 0), (3, 1), (3, 2), (4, 0), (4, 1), (4, 2), (4, 3)]
+
+    >>> keys, values = [], []
+    >>> for k, v in a.items():
+    ...     keys.append(k)
+    ...     values.append(v)
+    >>> keys
+    [(1, 0), (2, 0), (2, 1), (3, 0), (3, 1), (3, 2), (4, 0), (4, 1), (4, 2), (4, 3)]
+    >>> values
+    [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+
+    >>> a.mindim(1)
+    2
+    >>> a.mindim(2)
+    3
+    >>> a.mindim(3)
+    3
+    >>> a.mindim(4)
+    4
+    >>> a.mindim(5)
+    4
+    >>> a.mindim(6)
+    4
+    >>> a.mindim(7)
+    5
+    >>> a.mindim(8)
+    5
+    >>> a.mindim(9)
+    5
+    >>> a.mindim(10)
+    5
+    """
     def __init__(self, dim):
         super(SubdiagonalArray, self).__init__(dim)
         self.__dim = dim
@@ -378,3 +825,10 @@ class SubdiagonalArray(FlatStoredArray):
             res[i,i] = fill
             if not symmetric: res[i,i+1:self.__dim] = fill
         return res
+
+def _test():
+    import doctest
+    doctest.testmod(verbose=True)
+
+if __name__ == '__main__':
+    _test()
